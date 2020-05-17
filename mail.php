bf1077dee226862e017ee2d456d32b0366a1bd7c
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-05-17 13:12:54 +0300

    Add new php form

--- mail.php
+++ mail.php
@@ -1,31 +1,24 @@
-<?php 
- 
-	if(isset($_POST['username'], $_POST['email'], $_POST['tel'], $_POST['message'])){
-		
-		$to = "najoull19@gmail.com"; //this is your Email address
-		$from = $_POST['username'];
-		$username = $_POST['username'];
-		$email = $_POST['email'];
-		$tel = $_POST['tel'];
-		$message = $_POST['message'];
+<?php
+if (isset($_POST['username'], $_POST['email'], $_POST['tel'], $_POST['message'])) {
+	$to = "jacob.papageorgiou@gmail.com"; //this is your Email address
+	$from = $_POST['username'];
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$tel = $_POST['tel'];
+	$message = $_POST['message'];
 
 
-		$subject = "Form submission";
-		$message = $username . "\n" . $email . "\n" . $tel . "\n" . $message;
+	$subject = "Form submission";
+	$message = $username . "\n" . $email . "\n" . $tel . "\n" . $message;
 
-		$headers = "From:" . $from;
+	$headers = "From:" . $from;
 
 
-		if (mail($to, $subject, $message, $headers)) {
+	if (mail($to, $subject, $message, $headers)) {
 
-		    echo "success";
+		echo "success";
+	} else {
 
-		} else {
-
-			echo "failed";
-
-		}
-
-	}		
-
-?>
+		echo "failed";
+	}
+}
